71bc0093802537d9eee6bc9f609267cea19e13c7
Max Rozdobudko <max.rozdobudko@gmail.com>
2018-04-17 09:31:30 +0300

    Adds AIR runtime framework into the project to be independent on developer environment.

---
+++ anx-bridge-xcode/Vendor/Adobe/AIR/Adobe AIR.framework/Versions/1.0/Headers/Adobe AIR.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5c42cfc5526821d91e3a30f275b609a0794aa778c91b85fcf1771fcda4a85149
+size 788
---
+++ anx-bridge-xcode/Vendor/Adobe/AIR/Adobe AIR.framework/Versions/1.0/Headers/FlashRuntimeExtensions.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8b44f38bb0511653234afcd61ae18976efc1f9afd7f954e195c52d5a4ab51a2
+size 19796
